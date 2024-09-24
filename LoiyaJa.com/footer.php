43c6cdc17d8ab80f08d41c01f1fb3612d26f56d6
Md Rased Hasan Rokon <98162817+github-rokon@users.noreply.github.com>
2024-09-24 23:00:16 +0600

    Delete LoiyaJa.com directory

--- LoiyaJa.com/footer.php
+++
@@ -1,6 +0,0 @@
-<br><br>
-<center>
-<a href="about-us.php"><p style="color:black;">About Us</p></a><br>
-<p style="color:lack;">"<h3>Copyright 500BC-2024 by DevRashed. All Rights Reserved.</h3></p>
-</center>
-<br>
